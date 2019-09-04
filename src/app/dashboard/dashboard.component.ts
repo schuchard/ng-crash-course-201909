2deb2b7a418585b2e08fcaeb4d42c34f9c08761b
kevin schuchard <schuchard.kevin@gmail.com>
2019-09-04 13:51:48 -0600

    commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  @Input() customer: string;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
