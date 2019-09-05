19da90769c278eb74bb11e45dc53133a17faa0f5
kevin schuchard <schuchard.kevin@gmail.com>
2019-09-05 09:04:33 -0600

    remove

--- src/app/shared/alert.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AlertDirective } from './alert/alert.directive';
-
-describe('AlertDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AlertDirective();
-    expect(directive).toBeTruthy();
-  });
-});
