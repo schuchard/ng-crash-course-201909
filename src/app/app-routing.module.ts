2fecb3f3ceb4c6c8169b487567471f8799e3f12b
kevin schuchard <schuchard.kevin@gmail.com>
2019-09-06 10:42:30 -0600

    app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { ServiceExampleComponent } from './service-example/service-example.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./+admin/admin.module').then((mod) => mod.AdminModule),
+  },
+  {
+    path: 'home',
+    component: ReactiveFormComponent
+  },
+  {
+    path: 'service-example',
+    component: ServiceExampleComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,25 +15,6 @@ import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ServiceExampleComponent } from './service-example/service-example.component';
 
-const routes: Routes = [
-  {
-    path: 'admin',
-    loadChildren: () => import('./+admin/admin.module').then((mod) => mod.AdminModule),
-  },
-  {
-    path: 'home',
-    component: ReactiveFormComponent
-  },
-  {
-    path: 'service-example',
-    component: ServiceExampleComponent
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home',
-  }
-];
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,7 +32,6 @@ const routes: Routes = [
     SharedModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes),
     HttpClientModule
   ],
   providers: [],
