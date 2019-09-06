bde3503993374cfcdef5516a0e9f92d3a7b5e57c
kevin schuchard <schuchard.kevin@gmail.com>
2019-09-06 08:59:53 -0600

    add take

--- src/app/reactive-form/validators.ts
+++ src/app/reactive-form/validators.ts
@@ -2,10 +2,13 @@ import { FormControl, AsyncValidatorFn } from '@angular/forms';
 
 import { timer } from 'rxjs';
 
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 export const lengthAsyncValidator = (length = 2, time: number = 500): AsyncValidatorFn => {
   return (input: FormControl) => {
-    return timer(time).pipe(map(() => (input.value.length > length ? null : { lengthErr: true })));
+    return timer(time).pipe(
+      map(() => (input.value.length > length ? null : { lengthErr: true })),
+      take(1)
+    );
   };
 };
