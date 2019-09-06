1435571882b1dc7c66fde33f35e305cc06018955
kevin schuchard <schuchard.kevin@gmail.com>
2019-09-06 09:49:53 -0600

    rest service

---
+++ src/app/core/rest-service/rest-service.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RestServiceService {
+  constructor(private http: HttpClient) {}
+
+  get<T>(url: string): Observable<T> {
+    return this.http.get<T>(url);
+  }
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { RestServiceService as RestService } from '../core/rest-service/rest-service.service';
+
+interface Todos {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -11,11 +19,11 @@ export class DashboardComponent implements OnInit {
   @Input() customer: string;
   @Input() count: number;
 
-  todos$ = this.http
-    .get<any[]>('https://jsonplaceholder.typicode.com/todos')
+  todos$ = this.restService
+    .get<Todos[]>('https://jsonplaceholder.typicode.com/todos')
     .pipe(map((t) => t.slice(0, 10)));
 
-  constructor(private http: HttpClient) {}
+  constructor(private restService: RestService) {}
 
   ngOnInit() {}
 }
